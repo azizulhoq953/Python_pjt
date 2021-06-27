daf45ac18fe7a922654c984fd7c437d8a2729c9a
azizul_hoq <azizulhoq4305@gamil.com>
2021-06-27 19:57:33 +0600

    python-First-commit

---
+++ ponter.py
@@ -0,0 +1,33 @@
+
+a = 2
+print(a)
+
+print(isinstance(a, object))
+b=3
+
+print(b)
+# checking if string is an object or not
+print(isinstance(b, object))
+# assigning a list value
+inputList = [1,2,3]
+# checking if list is an object or not
+print(isinstance(inputList, object))
+#assigning a set value
+inputSet = {10,20,30}
+print(inputSet)
+# checking if set is an object or not
+print(isinstance(inputSet, object))
+# assigning a tuple value
+inputTuple = (100, 200, 300)
+print(inputTuple)
+#checking if tuple object or not
+print(isinstance(inputTuple, object))
+# assigning a dictionary value
+inputDict =   {
+"0": 1922,
+"1": "BMW",
+"2": 100
+}
+print(inputDict)
+#checking if  dictionary is an object or not
+print(isinstance(inputDict, object))
---
+++ ternary.py
@@ -0,0 +1,13 @@
+num1=60
+num2=50
+num3=70
+
+if num1>num2:
+print (num1)
+
+elif num2 > num3:
+
+print (num2)
+
+else:
+    print (num3)
